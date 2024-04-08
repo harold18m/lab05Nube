92f3c2a8c77f5ccd69b640ee7b5aafd26003279b
Harold Medrano <118697566+harold18m@users.noreply.github.com>
2024-04-08 10:33:55 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'userdemo';
-const MONGO_PASSWORD = 'Tecsup';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'deliverydb';
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
